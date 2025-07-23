3c21df8de52fc5bde40aff39e48e76ec1f70bba4
mpeters39 <mrp2342@gmail.com>
2025-07-23 17:19:16 -0400

    Changed ship class logic to register vertical movement instead of horizontal

--- ship.py
+++ ship.py
@@ -12,13 +12,14 @@ class Ship:
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.transform.rotate(self.image, -90)
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midleft = self.screen_rect.midleft
 
-        # Store a float for the ship's exact horizontal position.
-        self.x = float(self.rect.x)
+        # Store a float for the ship's exact *vertical* position.
+        self.y = float(self.rect.y)
 
         # Movement flags; start with a ship that's not moving.
         self.moving_down = False
@@ -26,19 +27,19 @@ class Ship:
 
     def center_ship(self):
         """Center the ship on the screen."""
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midleft = self.screen_rect.midleft
         self.x = float(self.rect.x)
 
     def update(self):
         """Update the ship's position based on movement flags."""
         # Update the ship's x value, not the rect.
-        if self.moving_down and self.rect.right < self.screen_rect.right:
-            self.x += self.settings.ship_speed
-        if self.moving_up and self.rect.left > 0:
-            self.x -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
             
-        # Update rect object from self.x.
-        self.rect.x = self.x
+        # Update rect object from self.y.
+        self.rect.y = self.y
 
     def blitme(self):
         """Draw the ship at its current location."""
